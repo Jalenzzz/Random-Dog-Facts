d6851f441ae41fe1903480b75ad8b9b135cc6ea2
Jalen <mnenejalen72@gmail.com>
2024-04-12 23:42:32 +0300

    Updated code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function fetchDogFact() {
   const response = await fetch("https://dog-api.kinduff.com/api/facts");
   const data = await response.json();
 
-  // Assuming 'facts' is an array in the API response
+  // facts is an array in the API response
   const randomFact = data.facts[0];
   factDisplay.textContent = randomFact;
 }
