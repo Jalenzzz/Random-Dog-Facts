33b9444b0039d74cac027f476f2c7fe7e2fa02ab
Jalen Mnene <mnenejalen72@gmail.com>
2024-05-22 19:34:25 +0300

    Added a woof sound effect and button timeout

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const factDisplay = document.getElementById("fact");
 const fetchButton = document.getElementById("fetch-fact");
+const woofSound = document.getElementById("woof-sound");
 
-fetchButton.addEventListener("click", fetchDogFact);
+fetchButton.addEventListener("click", async () => {
+  fetchButton.textContent = "Please wait...";
+  fetchButton.disabled = true; // Disable the button
+  fetchButton.classList.add("disabled-button");
+  await fetchDogFact();
+  woofSound.play(); // bark sound effect once a fact is fetched
+  setTimeout(() => {
+    fetchButton.textContent = "Fetch another fact!";
+    fetchButton.disabled = false; // Enable the button
+    fetchButton.classList.remove("disabled-button");
+  }, 3000);
+});
 
 async function fetchDogFact() {
   const response = await fetch("https://dog-api.kinduff.com/api/facts");
   const data = await response.json();
-
-  // facts is an array in the API response
   const randomFact = data.facts[0];
   factDisplay.textContent = randomFact;
 }
